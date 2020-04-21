e6347e56952a605b93f3d6f71462adf3b3b9a07c
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-04-20 19:08:09 -0500

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("JS-intro");
+
+const adder = (num)=>{
+
+    console.log(`${num + 42}`);
+}
+
+
+adder(32);
+
+
+
+const ageIn2099 = (year) => {
+    console.log(`Age in 2099 will be ${2099 - year}`);
+}
+
+ageIn2099(1983);
